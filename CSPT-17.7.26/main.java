690119e9a2e45c1a5e436961d472ff601d7a0a8d
harshini-4i <harshinicherry24@gmail.com>
2026-07-17 10:59:16 +0000

    boolean in java

---
+++ CSPT-17.7.26/main.java
@@ -0,0 +1,13 @@
+void main(){
+    int x = 0;
+    boolean b = true;
+    boolean f = false;
+    IO.println(b);
+    char ch = 'c';
+}
+
+/**
+ * print the max and min values of integer
+ * accept hexadecimal value, octalvalue and convert to integer
+ * perform summation of hexa and octal (use only integer method sum)
+ * */
